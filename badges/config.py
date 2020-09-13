e49df1b1d19f3aa48c7acf5655d57d5d1193bfe5
Nabarun Pal <pal.nabarun95@gmail.com>
2020-09-13 17:50:33 +0530

    Add config module
    Signed-off-by: Nabarun Pal <pal.nabarun95@gmail.com>

---
+++ badges/config.py
@@ -0,0 +1,10 @@
+import os
+
+
+class Config:
+    SECRET_KEY = (
+        os.getenv("SECRET_KEY")
+        or "equity~reprogram~unworried~splendid~deviation~width~pungent~awkward"
+    )
+    SQLALCHEMY_DATABASE_URI = os.getenv("BADGES_DSN") or "postgres:///badges"
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
